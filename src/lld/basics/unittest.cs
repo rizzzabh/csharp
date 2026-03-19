cedca9d6feae05b89d0ef00e317158d10c8c6d0f
rizzzabh <rishabhmishra963010@gmail.com>
2026-03-19 22:27:34 +0530

    add unittest

---
+++ src/lld/basics/unittest.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using Moq;
+
+public class CheckoutControllerTests
+{
+    [Fact]
+    public async Task ProcessCheckout_WithFestivalDiscount_ReturnsCorrectPrice()
+    {
+        Mock<IItemRepository> mockRepo = new Mock<IItemRepository>();
+        
+        mockRepo.Setup(repo => repo.GetItemPrice()).ReturnsAsync(100.0);
+ 
+        DiscountFactory realFactory = new DiscountFactory();
+
+        CheckoutController checkoutController = new CheckoutController(mockRepo.Object ,realFactory) ;
+        double finalPrice = await checkoutController.ProcessCheckout(1 , "FesTival") ;
+
+         Assert.Equal(80.0, finalPrice);
+    }
+}
