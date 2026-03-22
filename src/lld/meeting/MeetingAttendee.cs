baff198686e098b45e45ac2cb6e3d270ebe7b4f0
rizzzabh <rishabhmishra963010@gmail.com>
2026-03-22 15:57:47 +0530

    shift to linq

--- src/lld/meeting/MeetingAttendee.cs
+++ src/lld/meeting/MeetingAttendee.cs
@@ -16,4 +16,11 @@ public class MeetingAttendee
   public int MeetingId {get ; set ; }
 
   public Statusu RSVPStatus {get ; set ; }
+
+  public int GetPendingInviteCount(List<MeetingAttendee> allAttendees , int targetUserId)
+  {
+    int count = allAttendees.Where(a => a.UserId == targetUserId && a.RSVPStatus==Statusu.Pending).Count();
+
+    return count ; 
+  }
 }
